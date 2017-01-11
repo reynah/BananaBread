46faf2f8c7e3f183c054de45a5e72349614c0b3f
Darlene O <Darlene O>
2017-01-11 11:38:34 -0500

    commit updated changes

---
+++ src/MainPkg/MainClass.java
@@ -0,0 +1,20 @@
+
+package MainPkg;
+/**
+ * 
+ * Author: Darlene January 2017
+ */
+  public class MainClass {
+      
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+
