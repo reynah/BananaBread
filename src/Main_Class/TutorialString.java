33483f2474aa82f797d281ab8250090f05efd5ed
Darlene O <Darlene O>
2017-01-11 11:16:55 -0500

    initial commit

---
+++ src/Main_Class/TutorialString.java
@@ -0,0 +1,25 @@
+/*
+ *
+ *
+ * 
+ */
+package Main_Class;
+
+/**
+ * Darlene January 2017
+ */
+  class TutorialString {
+    public static void main(String[] args){
+        String foo = "Here is a string of information";
+        System.out.println(foo);
+        }
+    }
+    
+class DoMath {
+    public static void main(String[] args) {
+        double score = 1.0+2.0*3.0;
+        System.out.prinln(score);
+        score = score/2.0;
+        System.out.println(score);
+    }
+}
