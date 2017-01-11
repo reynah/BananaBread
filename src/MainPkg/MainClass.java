6881c2d62398dc09954286dc0967510f5db35e08
Darlene O <Darlene O>
2017-01-11 12:17:49 -0500

    committing tutorial 1 - 5

--- src/MainPkg/MainClass.java
+++ src/MainPkg/MainClass.java
@@ -5,16 +5,32 @@ package MainPkg;
  * Author: Darlene January 2017
  */
   public class MainClass {
-      
+  
+/**
+ * public static void main is my main method required in every MainClass
+ * It is a system defined argument for the Package so the program knows where to start
+ * you cannot use "main" in any other method in the Package
+ * 
+ * @param args
+ */
     public static void main(String[] args){
         System.out.println("Hello World");
         
+        int myNumber = 44;
+        System.out.println(myNumber);
         
+        String text = "Hello";
+        String blank = " ";
+        String name = "Bob";
+        String greeting = text + blank + name;
         
+        System.out.println(text+blank+name);
+        System.out.println(greeting);
+        System.out.println("My Number is: " + myNumber);
+
         
-        
-        
-        
+
+                
         }
     }
 
